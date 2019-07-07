93123846a6c942702eb15bfe732f6d3224927900
Jonas J C Sousa <jj_jcs@hotmail.com>
2019-07-06 23:44:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const DEFAULT_QUERY = 'redux';
 const DEFAULT_HPP = '100';
 
-const PATH_BASE = 'http://hn.algolia.com/api/v1';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
